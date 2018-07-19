84251cadec480738cedb571f8e27090f3216f1c5
Adam Norris <acnorrisuk@gmail.com>
2018-07-19 20:53:16 +0100

    fix footer spacing

--- front-page.php
+++ front-page.php
@@ -3,11 +3,11 @@
 get_header(); ?>
 
 <div class="home-intro">
-    <img src="//placehold.it/800x500" alt="">
     <div class="home-intro__text">
         <p class="lead">In perferendis quasi mollitia sapiente maxime similique incidunt </p>
         <p class="text-light">Lorem ipsum, dolor sit amet consectetur adipisicing elit. In perferendis quasi mollitia sapiente maxime similique incidunt assumenda deleniti, voluptates vel repellendus.</p>
     </div>
+    <img class="home-intro__image" src="<?php echo get_template_directory_uri();?>/img/adam-norris.jpg" alt="">
 </div>
 
 <?php get_footer(); ?>
