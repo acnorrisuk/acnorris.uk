9ae4910db0f3fc8f73c63ca500a6deb2e9ee71e1
Adam Norris <acnorrisuk@gmail.com>
2018-07-19 20:19:22 +0100

    base and homepage styles

--- front-page.php
+++ front-page.php
@@ -2,27 +2,11 @@
 
 get_header(); ?>
 
-<div class="pad home-grid">
-    <header class="masthead">
-
-        <!-- <div>
-            <a class="home-link" href="<?php echo esc_url(site_url('/')); ?>">
-                <p class="masthead__logo"><?php bloginfo('name');?></p>
-                <p class="masthead__tagline"><?php bloginfo('description');?></p>
-            </a>
-        </div> -->
-        <nav class="main-navigation">
-            <ul>
-                <li><a href="/acnorrisuk"><span aria-hidden="true">&#8627;</span> Home</a></li>
-                <li><a href="blog"><span aria-hidden="true">&#8627;</span> Blog</a></li>
-                <li><a href="about"><span aria-hidden="true">&#8627;</span> About</a></li>
-            </ul>
-        </nav>
-
-    </header>
-    <div class="primary">
-        <h1 class="page-title"><?php the_title(); ?></h1>
-        <p class="lead">I am a front-end developer currently working for a digital agency based in Leeds, UK.</p>
+<div class="home-intro">
+    <img src="//placehold.it/800x500" alt="">
+    <div class="home-intro__text">
+        <p class="lead">In perferendis quasi mollitia sapiente maxime similique incidunt </p>
+        <p class="text-light">Lorem ipsum, dolor sit amet consectetur adipisicing elit. In perferendis quasi mollitia sapiente maxime similique incidunt assumenda deleniti, voluptates vel repellendus.</p>
     </div>
 </div>
 
--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html class="no-js" lang="en-GB">
+<html lang="en-GB">
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
@@ -7,11 +7,32 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="apple-touch-icon" href="apple-touch-icon.png">
         <link rel="manifest" href="<?php echo get_template_directory_uri();?>/manifest.json">
-        <link href="https://fonts.googleapis.com/css?family=Monoton" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css?family=Bungee+Inline|Bungee+Shade|Gravitas+One|Peralta|Ribeye+Marrow" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Bitter:400,400i,700" rel="stylesheet">
 
     <?php wp_head(); ?>
     </head>
+
     <body <?php body_class(); ?>>
 
-        <main>
+        <div class="site-content">
+
+            <header class="masthead">
+
+                <div class="masthead__group">
+
+                    <h1 class="masthead__logo"><a class="plain-link" href="<?php echo esc_url(site_url('/')); ?>">Adam Norris</a></h1>
+
+                    <nav class="masthead__nav text-light">
+                        <ul class="list-reset list-inline">
+                            <li><a class="plain-link" href="about">About</a></li>
+                            <li><a class="plain-link" href="blog">Blog</a></li>
+                        </ul>
+                    </nav>   
+                
+                </div>
+                
+                <hr>
+
+            </header>
+
+            <main class="main">
