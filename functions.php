138f7a77dec25c15841c259022441f0da91c9289
Adam Norris <acnorrisuk@gmail.com>
2018-07-23 23:03:20 +0100

    remove GA

--- functions.php
+++ functions.php
@@ -42,23 +42,6 @@ function acnorrisuk_scripts() {
 add_action( 'wp_enqueue_scripts', 'acnorrisuk_scripts');
 
 /*
-* Add Google Analytics
-*/
-function add_google_analytics() { ?>
-	<script>
-	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-	ga('create', 'UA-54325837-1', 'auto');
-	ga('send', 'pageview');
-
-	</script>
-<?php }
-add_action('wp_head', 'add_google_analytics');
-
-/*
 * Disables all emoji actions
 */
 function disable_wp_emojicons() {
