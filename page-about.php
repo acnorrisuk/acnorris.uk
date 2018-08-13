861e1b05d34da8d66620f9f6c759a57c072b2659
Adam Norris <acnorrisuk@gmail.com>
2018-08-13 22:38:50 +0100

    update text on home / about pages

--- page-about.php
+++ page-about.php
@@ -5,9 +5,10 @@ get_header(); ?>
     <div class="primary">
         <h1 class="page-title"><?php the_title(); ?></h1>
             <h2>Who am I?</h2>
-            <p>My name is Adam Norris and I'm a front-end developer currently working for a digital agency based in Leeds, UK. I am married with two children and enjoy music, table-top gaming and the odd curry.</p>
+            <p>My name is Adam Norris and I'm a front-end developer currently working for Harris: a digital agency based in Leeds, UK. I am married with two children and in my spare time enjoy playing the guitar / piano, table-top gaming and the odd curry.</p>
             <h2>My Philosophy</h2>
-            <p>I enjoy building websites which are accessible, performant and have a great user experience. I prefer to focus on what is best for the user rather than this year's latest trend. I use the principles of progressive enhancement to create clean, semantic code which will work across a range of situations.</p>
+            <p>I enjoy building websites which are accessible, performant and have a great user experience. I prefer to focus on what is best for the user rather than this year's latest trend. I use the principles of progressive enhancement to create clean, semantic code which will work across a range of different situations.</p>
+            <p>I like to keep up to date with what's happening in the industry and spend time reading blog posts, listening to podcasts and watching online tutorials to stay informed about new approaches and best practices.</p>
             <h2>My Toolbox</h2>
             <p>I try to keep things simple and only use tools when they're really needed. Here's some I use on a regular basis:</p>
             <ul>
