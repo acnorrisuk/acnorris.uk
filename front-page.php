0efd9b22688e5e700e882ddd61baf6a69f94a3b5
Adam Norris <acnorrisuk@gmail.com>
2018-08-18 21:54:58 +0100

    move image to about page

--- front-page.php
+++ front-page.php
@@ -2,12 +2,9 @@
 
 get_header(); ?>
 
-<div class="home-intro">
-    <div class="home-intro__text">
-        <p class="lead">Hi, I'm Adam: a front-end developer based in Leeds, UK</p>
-        <p>I design and build websites with a focus on accessibility and performance. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque, perferendis rerum.</p>
-    </div>
-    <img class="home-intro__image" src="<?php echo get_template_directory_uri();?>/img/adam-norris.jpg" alt="">
+<div class="primary">
+    <p class="lead">Hi, I'm Adam: a front-end developer based in Leeds, UK</p>
+    <p>I design and build websites with a focus on accessibility and performance. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque, perferendis rerum.</p>
 </div>
 
 <?php get_footer(); ?>
--- page-about.php
+++ page-about.php
@@ -4,24 +4,25 @@ get_header(); ?>
 
     <div class="primary">
         <h1 class="page-title"><?php the_title(); ?></h1>
-            <h2>Who am I?</h2>
-            <p>My name is Adam Norris and I'm a front-end developer currently working for Harris: a digital agency based in Leeds, UK. I am married with two children and in my spare time enjoy playing the guitar / piano, table-top gaming and the odd curry.</p>
-            <h2>My Philosophy</h2>
-            <p>I enjoy building websites which are accessible, performant and have a great user experience. I prefer to focus on what is best for the user rather than this year's latest trend. I use the principles of progressive enhancement to create clean, semantic code which will work across a range of different situations.</p>
-            <p>I like to keep up to date with what's happening in the industry and spend time reading blog posts, listening to podcasts and watching online tutorials to stay informed about new approaches and best practices.</p>
-            <h2>My Toolbox</h2>
-            <p>I try to keep things simple and only use tools when they're really needed. Here's some I use on a regular basis:</p>
-            <ul>
-            <li>WordPress / Craft CMS</li>
-            <li>Photoshop / Sketch</li>
-            <li>VS Code</li>
-            <li>Git</li>
-            <li>Github / Bitbucket</li>
-            <li>Gulp / Codekit</li>
-            <li>Deploybot HQ</li>
-            <li>Google Analytics</li>
-            </ul>
-            <h2>Find Me</h2>
-            <p>You can find me on the various social channels below or alternatively, contact me at <a href="mailto:acnorrisuk@gmail.com">acnorrisuk@gmail.com</a></p>
-        </div> 
+        <h2>Who am I?</h2>
+        <p>My name is Adam Norris and I'm a front-end developer currently working for Harris: a digital agency based in Leeds, UK. I am married with two children and in my spare time enjoy playing the guitar / piano, table-top gaming and the odd curry.</p>
+        <img class="blend" src="<?php echo get_template_directory_uri();?>/img/adam-norris.jpg" alt="">
+        <h2>My Philosophy</h2>
+        <p>I enjoy building websites which are accessible, performant and have a great user experience. I prefer to focus on what is best for the user rather than this year's latest trend. I use the principles of progressive enhancement to create clean, semantic code which will work across a range of different situations.</p>
+        <p>I like to keep up to date with what's happening in the industry and spend time reading blog posts, listening to podcasts and watching online tutorials to stay informed about new approaches and best practices.</p>
+        <h2>My Toolbox</h2>
+        <p>I try to keep things simple and only use tools when they're really needed. Here's some I use on a regular basis:</p>
+        <ul>
+        <li>WordPress / Craft CMS</li>
+        <li>Photoshop / Sketch</li>
+        <li>VS Code</li>
+        <li>Git</li>
+        <li>Github / Bitbucket</li>
+        <li>Gulp / Codekit</li>
+        <li>Deploybot HQ</li>
+        <li>Google Analytics</li>
+        </ul>
+        <h2>Find Me</h2>
+        <p>You can find me on the various social channels below or alternatively, contact me at <a href="mailto:acnorrisuk@gmail.com">acnorrisuk@gmail.com</a></p>
+    </div> 
 <?php get_footer(); ?>
